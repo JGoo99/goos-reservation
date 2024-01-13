f559b6f2305f607fa6ef77efc0a5027c50d6437e
JGoo99 <zzgg3040@naver.com>
2024-01-13 16:34:48 +0900

    create project

---
+++ src/main/java/com/goo99/goosreservation/GoosReservationApplication.java
@@ -0,0 +1,13 @@
+package com.goo99.goosreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoosReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoosReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goo99/goosreservation/GoosReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goo99.goosreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoosReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
